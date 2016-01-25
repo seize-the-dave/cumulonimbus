2d7b8be89d2bde1687b2de974e962e9ae9581e18
Dave Grant <dgrant@sorensonmedia.com>
2016-01-25 13:41:19 +0000

    Added more methods for VPC.

---
+++ test/vpc-test.js
@@ -0,0 +1,65 @@
+var should = require('should'),
+    cn = require('../lib/cumulonimbus');
+
+describe('VPC', function() {
+  describe('instantiating an empty VPC', function() {
+    it('we only get the type key', function() {
+      var resource = new cn.EC2.VPC("VPC");
+      should(resource.toJson()).deepEqual({
+        "Type": "AWS::EC2::VPC"
+      });
+    });
+  });
+
+  describe('setting a CIDR block', function() {
+    it('should be in the JSON output', function() {
+      var resource = new cn.EC2.VPC("VPC");
+      resource.setCidrBlock("10.0.0.0/24");
+      should(resource.toJson()).deepEqual({
+        "Type": "AWS::EC2::VPC",
+        "Properties": {
+          "CidrBlock": "10.0.0.0/24"
+        }
+      });
+    });
+  });
+
+  describe('enabling DNS hostnames', function() {
+    it('should be in the JSON output', function() {
+      var resource = new cn.EC2.VPC("VPC");
+      resource.enableDnsHostnames(true);
+      should(resource.toJson()).deepEqual({
+        "Type": "AWS::EC2::VPC",
+        "Properties": {
+          "EnableDnsHostnames": true
+        }
+      });
+    });
+  });
+
+  describe('enabling DNS support', function() {
+    it('should be in the JSON output', function() {
+      var resource = new cn.EC2.VPC("VPC");
+      resource.enableDnsSupport(true);
+      should(resource.toJson()).deepEqual({
+        "Type": "AWS::EC2::VPC",
+        "Properties": {
+          "EnableDnsSupport": true
+        }
+      });
+    });
+  });
+
+  describe('setting instance tenancy', function() {
+    it('should be in the JSON output', function() {
+      var resource = new cn.EC2.VPC("VPC");
+      resource.setInstanceTenancy("dedicated");
+      should(resource.toJson()).deepEqual({
+        "Type": "AWS::EC2::VPC",
+        "Properties": {
+          "InstanceTenancy": "dedicated"
+        }
+      });
+    });
+  });
+});
