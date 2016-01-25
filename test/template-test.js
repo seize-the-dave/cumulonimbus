271f0864416c7b39386d54aaf9ec57ac8c44939e
David Grant <david@grant.org.uk>
2016-01-25 10:06:46 +0000

    Added initial validation.

--- test/template-test.js
+++ test/template-test.js
@@ -1,14 +1,26 @@
 var vows = require('vows'),
-    assert = require('assert'),
-    cn = require('../lib/cumulonimbus');
-    
-vows.describe('JSON Rendering').addBatch({
-    'when instantiating a new template': {
-        topic: function() {
-            return new cn.Template().toJson();
-        },
-        'we get only the template format version': function(topic) {
-            assert.equal(JSON.stringify(topic), JSON.stringify({"AWSTemplateFormatVersion": "2010-09-09"}));
-        }
+  assert = require('assert'),
+  cn = require('../lib/cumulonimbus');
+
+vows.describe('Templates').addBatch({
+  'when instantiating a new template': {
+    topic: function() {
+      return new cn.Template().toJson();
+    },
+    'we get only the template format version': function(topic) {
+      assert.equal(JSON.stringify(topic), JSON.stringify({
+        "AWSTemplateFormatVersion": "2010-09-09"
+      }));
     }
+  },
+  'when validating a template': {
+    topic: function() {
+      return new cn.Template();
+    },
+    "an error is returned when we don't add any resources": function(topic) {
+      topic.validate(function(err) {
+        assert.isNotNull(err);
+      });
+    }
+  }
 }).export(module);
