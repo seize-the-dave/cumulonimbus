b71f03919c712be12840f7e2a0cd4e48d3188342
Dave Grant <dgrant@sorensonmedia.com>
2016-01-26 15:44:33 +0000

    Completed AWS::EC2::SecurityGroup

--- test/security-group-test.js
+++ test/security-group-test.js
@@ -1,5 +1,5 @@
 var should = require('should'),
-    cn = require('../lib/cumulonimbus');
+  cn = require('../lib/cumulonimbus');
 
 describe('AWS::EC2::SecurityGroup', function() {
   describe('New Instance', function() {
@@ -69,6 +69,281 @@ describe('AWS::EC2::SecurityGroup', function() {
             "Name": "MySecurityGroup"
           }
         }
+      });
+    });
+  });
+
+  describe('SecurityGroupEgress', function() {
+    describe('CidrIp', function() {
+      it('should be present in the JSON output', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var egress = new cn.Ec2.SecurityGroup.Egress();
+        egress.setCidrIp("10.0.0.0/24");
+        resource.setSecurityGroupEgress([egress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupEgress": [{
+              "CidrIp": "10.0.0.0/24"
+            }]
+          }
+        });
+      });
+    });
+
+    describe('FromPort', function() {
+      it('should be present in the JSON output', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var egress = new cn.Ec2.SecurityGroup.Egress();
+        egress.setFromPort(22);
+        resource.setSecurityGroupEgress([egress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupEgress": [{
+              "FromPort": 22
+            }]
+          }
+        });
+      });
+    });
+
+    describe('IpProtocol', function() {
+      it('should be present in the JSON output', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var egress = new cn.Ec2.SecurityGroup.Egress();
+        egress.setIpProtocol("tcp");
+        resource.setSecurityGroupEgress([egress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupEgress": [{
+              "IpProtocol": "tcp"
+            }]
+          }
+        });
+      });
+    });
+
+    describe('DestinationSecurityGroupId', function() {
+      it('accepts reference', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var egress = new cn.Ec2.SecurityGroup.Egress();
+        egress.setDestinationSecurityGroupId(resource);
+        resource.setSecurityGroupEgress([egress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupEgress": [{
+              "DestinationSecurityGroupId": {
+                "Ref": "MySecurityGroup"
+              }
+            }]
+          }
+        });
+      });
+
+      it('accepts string', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var egress = new cn.Ec2.SecurityGroup.Egress();
+        egress.setDestinationSecurityGroupId("sg-1234567");
+        resource.setSecurityGroupEgress([egress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupEgress": [{
+              "DestinationSecurityGroupId": "sg-1234567"
+            }]
+          }
+        });
+      });
+
+      it('rejects malformed string', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var egress = new cn.Ec2.SecurityGroup.Egress();
+        should.throws(function() {
+          egress.setDestinationSecurityGroupId("group-1234567");
+        });
+      });
+    });
+
+    describe('ToPort', function() {
+      it('should be present in the JSON output', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var egress = new cn.Ec2.SecurityGroup.Egress();
+        egress.setToPort(22);
+        resource.setSecurityGroupEgress([egress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupEgress": [{
+              "ToPort": 22
+            }]
+          }
+        });
+      });
+    });
+  });
+
+  describe('SecurityGroupIngress', function() {
+    describe('CidrIp', function() {
+      it('should be present in the JSON output', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var ingress = new cn.Ec2.SecurityGroup.Ingress();
+        ingress.setCidrIp("10.0.0.0/24");
+        resource.setSecurityGroupIngress([ingress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupIngress": [{
+              "CidrIp": "10.0.0.0/24"
+            }]
+          }
+        });
+      });
+    });
+
+    describe('FromPort', function() {
+      it('should be present in the JSON output', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var ingress = new cn.Ec2.SecurityGroup.Ingress();
+        ingress.setFromPort(22);
+        resource.setSecurityGroupIngress([ingress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupIngress": [{
+              "FromPort": 22
+            }]
+          }
+        });
+      });
+    });
+
+    describe('IpProtocol', function() {
+      it('should be present in the JSON output', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var ingress = new cn.Ec2.SecurityGroup.Ingress();
+        ingress.setIpProtocol("tcp");
+        resource.setSecurityGroupIngress([ingress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupIngress": [{
+              "IpProtocol": "tcp"
+            }]
+          }
+        });
+      });
+    });
+
+    describe('ToPort', function() {
+      it('should be present in the JSON output', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var ingress = new cn.Ec2.SecurityGroup.Ingress();
+        ingress.setToPort(22);
+        resource.setSecurityGroupIngress([ingress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupIngress": [{
+              "ToPort": 22
+            }]
+          }
+        });
+      });
+    });
+
+    describe('SourceSecurityGroupId', function() {
+      it('accepts reference', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var ingress = new cn.Ec2.SecurityGroup.Ingress();
+        ingress.setSourceSecurityGroupId(resource);
+        resource.setSecurityGroupIngress([ingress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupIngress": [{
+              "SourceSecurityGroupId": {
+                "Ref": "MySecurityGroup"
+              }
+            }]
+          }
+        });
+      });
+
+      it('accepts string', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var ingress = new cn.Ec2.SecurityGroup.Ingress();
+        ingress.setSourceSecurityGroupId("sg-1234567");
+        resource.setSecurityGroupIngress([ingress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupIngress": [{
+              "SourceSecurityGroupId": "sg-1234567"
+            }]
+          }
+        });
+      });
+
+      it('rejects malformed string', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var egress = new cn.Ec2.SecurityGroup.Ingress();
+        should.throws(function() {
+          egress.setSourceSecurityGroupId("group-1234567");
+        });
+      });
+    });
+
+    describe('SourceSecurityGroupName', function() {
+      it('accepts reference', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var ingress = new cn.Ec2.SecurityGroup.Ingress();
+        ingress.setSourceSecurityGroupName(resource);
+        resource.setSecurityGroupIngress([ingress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupIngress": [{
+              "SourceSecurityGroupName": {
+                "Ref": "MySecurityGroup"
+              }
+            }]
+          }
+        });
+      });
+
+      it('accepts string', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var ingress = new cn.Ec2.SecurityGroup.Ingress();
+        ingress.setSourceSecurityGroupName("MySecurityGroup");
+        resource.setSecurityGroupIngress([ingress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupIngress": [{
+              "SourceSecurityGroupName": "MySecurityGroup"
+            }]
+          }
+        });
+      });
+    });
+
+    describe('SourceSecurityGroupOwnerId', function() {
+      it('should be present in the JSON output', function() {
+        var resource = new cn.Ec2.SecurityGroup("MySecurityGroup");
+        var ingress = new cn.Ec2.SecurityGroup.Ingress();
+        ingress.setSourceSecurityGroupOwnerId("12345678");
+        resource.setSecurityGroupIngress([ingress]);
+        should(resource.toJson()).deepEqual({
+          "Type": "AWS::EC2::SecurityGroup",
+          "Properties": {
+            "SecurityGroupIngress": [{
+              "SourceSecurityGroupOwnerId": "12345678"
+            }]
+          }
+        });
       });
     });
   });
