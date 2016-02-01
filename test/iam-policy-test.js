59388d9251ed2e15968b180f521f5aa9b4be9b7e
Dave Grant <dgrant@sorensonmedia.com>
2016-02-01 09:50:39 +0000

    Added Principal and NotPrincipal.

--- test/iam-policy-test.js
+++ test/iam-policy-test.js
@@ -19,7 +19,7 @@ describe('Policy', function() {
         "Version": "2012-10-17",
         "Id": "cd3ad3d9-2776-4ef1-a904-4c229d1642ee"
       });
-    })
+    });
   });
 
   describe('Statement', function() {
@@ -45,7 +45,7 @@ describe('Policy', function() {
             "Sid": "1"
           }]
         });
-      })
+      });
     });
 
     describe('Effect', function() {
@@ -136,7 +136,7 @@ describe('Policy', function() {
         }).should.throw(/Principal/);
       });
 
-      it('should reject AWS account ID', function() {
+      it('should reject invalid AWS account ID', function() {
         var policy = new Policy();
         var statement = new Policy.Statement();
         (function() {
@@ -180,11 +180,484 @@ describe('Policy', function() {
         });
       });
 
-      it('should reject non-string AWS principals (for now)', function() {
+      it('should accept ARN-format AWS account IDs', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setPrincipal({
+          "AWS": "arn:aws:iam::123456789012:root"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "Principal": {
+              "AWS": "arn:aws:iam::123456789012:root"
+            }
+          }]
+        });
+      });
+
+      it('should accept array of ARN-format AWS account IDs', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setPrincipal({
+          "AWS": [
+            "arn:aws:iam::123456789012:root",
+            "arn:aws:iam::123456789013:root"
+          ]
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "Principal": {
+              "AWS": [
+                "arn:aws:iam::123456789012:root",
+                "arn:aws:iam::123456789013:root"
+              ]
+            }
+          }]
+        });
+      });
+
+      it('should accept ARN-format AWS user IDs', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setPrincipal({
+          "AWS": "arn:aws:iam::123456789012:user/example"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "Principal": {
+              "AWS": "arn:aws:iam::123456789012:user/example"
+            }
+          }]
+        });
+      });
+
+      it('should reject ARN-format AWS user wildcard', function() {
         var policy = new Policy();
         var statement = new Policy.Statement();
         (function() {
           statement.setPrincipal({
+            "AWS": "arn:aws:iam::123456789012:user/*"
+          });
+        }).should.throw(/Principal/);
+      });
+
+      it('should accept array of ARN-format AWS user IDs', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setPrincipal({
+          "AWS": [
+            "arn:aws:iam::123456789012:user/foo",
+            "arn:aws:iam::123456789012:user/bar",
+          ]
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "Principal": {
+              "AWS": [
+                "arn:aws:iam::123456789012:user/foo",
+                "arn:aws:iam::123456789012:user/bar"
+              ]
+            }
+          }]
+        });
+      });
+
+      it('should reject array of ARN-format AWS user wildcard', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        (function() {
+          statement.setPrincipal({
+            "AWS": [
+              "arn:aws:iam::123456789012:user/*",
+              "arn:aws:iam::123456789012:user/bar"
+            ]
+          });
+        }).should.throw(/Principal/);
+      });
+
+      it('should accept web-identity federation', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setPrincipal({
+          "Federated": "accounts.google.com"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "Principal": {
+              "Federated": "accounts.google.com"
+            }
+          }]
+        });
+      });
+
+      it('should accept ARN-format AWS role', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setPrincipal({
+          "AWS": "arn:aws:iam::123456789012:role/example"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "Principal": {
+              "AWS": "arn:aws:iam::123456789012:role/example"
+            }
+          }]
+        });
+      });
+
+      it('should accept ARN-format AWS assumed-role user', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setPrincipal({
+          "AWS": "arn:aws:iam::123456789012:assumed-role/example/example"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "Principal": {
+              "AWS": "arn:aws:iam::123456789012:assumed-role/example/example"
+            }
+          }]
+        });
+      });
+
+      it('should accept AWS service', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setPrincipal({
+          "Service": "ec2.amazonaws.com"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "Principal": {
+              "Service": "ec2.amazonaws.com"
+            }
+          }]
+        });
+      });
+
+      it('should accept canonical user', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setPrincipal({
+          "CanonicalUser": "79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "Principal": {
+              "CanonicalUser": "79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be"
+            }
+          }]
+        });
+      });
+    });
+
+    describe('NotPrincipal', function() {
+      it('should accept wildcard', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal("*");
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": "*"
+          }]
+        });
+      });
+
+      it('should reject non-wildcard strings', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        (function() {
+          statement.setNotPrincipal("Invalid");
+        }).should.throw(/Principal/);
+      });
+
+      it('should reject more than zero keys', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        (function() {
+          statement.setNotPrincipal({
+          });
+        }).should.throw(/Principal/);
+      });
+
+      it('should reject more than one key', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        (function() {
+          statement.setNotPrincipal({
+            "AWS": "*",
+            "Federated": "Invalid"
+          });
+        }).should.throw(/Principal/);
+      });
+
+      it('should reject invalid key', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        (function() {
+          statement.setNotPrincipal({
+            "Example": "Invalid"
+          });
+        }).should.throw(/Principal/);
+      });
+
+      it('should reject invalid AWS account ID', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        (function() {
+          statement.setNotPrincipal({
+            "AWS": "12345678901"
+          });
+        }).should.throw(/Principal/);
+      });
+
+      it('should accept AWS account wildcard', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "AWS": "*"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "AWS": "*"
+            }
+          }]
+        });
+      });
+
+      it('should accept AWS account IDs', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "AWS": "123456789012"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "AWS": "123456789012"
+            }
+          }]
+        });
+      });
+
+      it('should accept ARN-format AWS account IDs', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "AWS": "arn:aws:iam::123456789012:root"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "AWS": "arn:aws:iam::123456789012:root"
+            }
+          }]
+        });
+      });
+
+      it('should accept array of ARN-format AWS account IDs', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "AWS": [
+            "arn:aws:iam::123456789012:root",
+            "arn:aws:iam::123456789013:root"
+          ]
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "AWS": [
+                "arn:aws:iam::123456789012:root",
+                "arn:aws:iam::123456789013:root"
+              ]
+            }
+          }]
+        });
+      });
+
+      it('should accept ARN-format AWS user IDs', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "AWS": "arn:aws:iam::123456789012:user/example"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "AWS": "arn:aws:iam::123456789012:user/example"
+            }
+          }]
+        });
+      });
+
+      it('should reject ARN-format AWS user wildcard', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        (function() {
+          statement.setNotPrincipal({
+            "AWS": "arn:aws:iam::123456789012:user/*"
+          });
+        }).should.throw(/Principal/);
+      });
+
+      it('should accept array of ARN-format AWS user IDs', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "AWS": [
+            "arn:aws:iam::123456789012:user/foo",
+            "arn:aws:iam::123456789012:user/bar",
+          ]
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "AWS": [
+                "arn:aws:iam::123456789012:user/foo",
+                "arn:aws:iam::123456789012:user/bar"
+              ]
+            }
+          }]
+        });
+      });
+
+      it('should reject array of ARN-format AWS user wildcard', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        (function() {
+          statement.setNotPrincipal({
+            "AWS": [
+              "arn:aws:iam::123456789012:user/*",
+              "arn:aws:iam::123456789012:user/bar"
+            ]
+          });
+        }).should.throw(/Principal/);
+      });
+
+      it('should accept web-identity federation', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "Federated": "accounts.google.com"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "Federated": "accounts.google.com"
+            }
+          }]
+        });
+      });
+
+      it('should accept ARN-format AWS role', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "AWS": "arn:aws:iam::123456789012:role/example"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "AWS": "arn:aws:iam::123456789012:role/example"
+            }
+          }]
+        });
+      });
+
+      it('should accept ARN-format AWS assumed-role user', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "AWS": "arn:aws:iam::123456789012:assumed-role/example/example"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "AWS": "arn:aws:iam::123456789012:assumed-role/example/example"
+            }
+          }]
+        });
+      });
+
+      it('should accept AWS service', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "Service": "ec2.amazonaws.com"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "Service": "ec2.amazonaws.com"
+            }
+          }]
+        });
+      });
+
+      it('should accept canonical user', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        statement.setNotPrincipal({
+          "CanonicalUser": "79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be"
+        });
+        policy.addStatement(statement);
+        should(policy.toJson()).deepEqual({
+          "Version": "2012-10-17",
+          "Statement": [{
+            "NotPrincipal": {
+              "CanonicalUser": "79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be"
+            }
+          }]
+        });
+      });
+
+      it('should reject non-string AWS principals (for now)', function() {
+        var policy = new Policy();
+        var statement = new Policy.Statement();
+        (function() {
+          statement.setNotPrincipal({
             "AWS": [
               "123456789012"
             ]
