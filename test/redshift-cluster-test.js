6955baef830aacf932ecb9e9088a42acba141490
David Grant <david@grant.org.uk>
2016-02-02 14:00:21 +0000

    Moved rest of the tests.

--- test/redshift-cluster-test.js
+++ test/redshift-cluster-test.js
@@ -10,4 +10,43 @@ describe('AWS::Redshift::Cluster', function() {
       });
     });
   });
+
+  describe('AllowVersionUpgrade', function() {
+    it('should accept boolean', function() {
+      var resource = new cn.Redshift.Cluster("MyCluster");
+      resource.setAllowVersionUpgrade(true);
+      should(resource.toJson()).deepEqual({
+        "Type": "AWS::Redshift::Cluster",
+        "Properties": {
+          "AllowVersionUpgrade": true,
+        }
+      });
+    });
+  });
+
+  describe('AutomatedSnapshotRetentionPeriod', function() {
+    it('should accept integer', function() {
+      var resource = new cn.Redshift.Cluster("MyCluster");
+      resource.setAutomatedSnapshotRetentionPeriod(7);
+      should(resource.toJson()).deepEqual({
+        "Type": "AWS::Redshift::Cluster",
+        "Properties": {
+          "AutomatedSnapshotRetentionPeriod": 7,
+        }
+      });
+    });
+  });
+
+  describe('AvailabilityZone', function() {
+    it('should accept string', function() {
+      var resource = new cn.Redshift.Cluster("MyCluster");
+      resource.setAvailabilityZone("us-east-1a");
+      should(resource.toJson()).deepEqual({
+        "Type": "AWS::Redshift::Cluster",
+        "Properties": {
+          "AvailabilityZone": "us-east-1a",
+        }
+      });
+    });
+  });
 });
